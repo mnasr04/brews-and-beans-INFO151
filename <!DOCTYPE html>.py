5f4e8c53433f0ee5bef28b6fc5f317a4ecb7c223
mnasr04 <mn688@drexel.edu>
2024-06-04 22:02:36 -0400

    Add files via upload

---
+++ <!DOCTYPE html>.py
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Simple HTML Document</title> 
+</head> 
+<body> 
+    <!-- #1. Top-level heading --> 
+    <h1 style="text-align: center;">Welcome to My Webpage</h1> 
+
+    <p style="background-color: cyan;">This is a paragraph with a cyan background.</p>
+
+    <ul>
+        <li>List item 1<li>
+        <li><strong>List item 2</strong></li> 
+    </ul> 
+
+    <table border="1">
+        <tr>
+            <td>Row 1, Column 1</td>
+        </tr>
+        <tr>
+            <td>Row 2, Column 1</td>
+        </tr>
+    </table>
+
+     <a href="paragraph" title="Click me to go to the paragraph above">Jump to Paragraph</a>
+
+     <p id="paragraph">Anchor for link jumping</p>
+</body>
+</html> 
